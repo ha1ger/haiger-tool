6c92abb4df32e620c34fd2e3df401e00d3f76bcc
Bestony <best.tony@foxmail.com>
2019-09-25 10:32:30 +0800

    feat: done function

--- miniprogram/pages/index.js
+++ miniprogram/pages/index.js
@@ -3,16 +3,18 @@ Page({
   data: {
 
   },
-
-  onLoad: function (options) {
-
-  },
-
-  onReady: function () {
-
-  },
-
   getInfo: function(e) {
-    console.log(e.detail.userInfo.avatarUrl)
+    // 调用云函数
+    wx.cloud.callFunction({
+      name:'image',
+      data:{
+        avatar: e.detail.userInfo.avatarUrl, // 头像获取自 userInfo
+        style: 2 // style 可以取值 1 ～ 4
+      }
+    }).then(res => {
+      this.setData({
+        avatarUrl:res.result.url
+      })
+    })
   }
 })
